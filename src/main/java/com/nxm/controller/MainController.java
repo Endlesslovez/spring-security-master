e42186bfc202eaf4703e165a9931e5fb7bef4101
Vu_T-Solution <leanhvu86@gmail.com>
2019-11-19 12:19:23 +0700

    EXPORT EXCEL

--- src/main/java/com/nxm/controller/MainController.java
+++ src/main/java/com/nxm/controller/MainController.java
@@ -1,8 +1,18 @@
 package com.nxm.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.activation.MimetypesFileTypeMap;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +23,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nxm.model.StockTotalDetail;
@@ -76,4 +87,79 @@ public class MainController {
 		return "quenmatkhau";
 	}
 
+	@RequestMapping("/exportExcel")
+    public void downloadFile(HttpServletRequest httpServletRequest, HttpServletResponse response) {             
+
+		String typeOflst = httpServletRequest.getParameter("typeOflst");
+		 String fileName ;
+		if (typeOflst.equals("1")) {
+			fileName = "F:/download/QLPM/DsSanPhamHethan.xls";
+		} else {
+			fileName = "F:/download/QLPM/DsSanPhamHetHang.xls";
+		}
+    try {
+
+        
+         HSSFWorkbook workbook = new HSSFWorkbook();
+         HSSFSheet sheet = workbook.createSheet("Danh Sách Nhân Viên");
+
+         HSSFRow rowhead = sheet.createRow((short) 0);
+         rowhead.createCell(0).setCellValue("No.");
+         rowhead.createCell(1).setCellValue("Name");
+         rowhead.createCell(2).setCellValue("Address");
+         rowhead.createCell(3).setCellValue("Email");
+
+         HSSFRow row = sheet.createRow((short) 1);
+         row.createCell(0).setCellValue("1");
+         row.createCell(1).setCellValue("Carlos");
+         row.createCell(2).setCellValue("Costa Rica");
+         row.createCell(3).setCellValue("myNameh@gmail.com");
+
+         FileOutputStream fileOut = new FileOutputStream(fileName);
+         workbook.write(fileOut);
+         fileOut.close();
+         System.out.println("Your excel file has been generated!");
+
+         //Code to download
+         File fileToDownload = new File(fileName);
+         InputStream in = new FileInputStream(fileToDownload);
+
+         // Gets MIME type of the file
+         String mimeType = new MimetypesFileTypeMap().getContentType(fileName);
+
+         if (mimeType == null) {
+             // Set to binary type if MIME mapping not found
+             mimeType = "application/octet-stream";
+         }
+         System.out.println("MIME type: " + mimeType);
+
+         // Modifies response
+         response.setContentType(mimeType);
+         response.setContentLength((int) fileToDownload.length());
+
+         // Forces download
+         String headerKey = "Content-Disposition";
+         String headerValue = String.format("attachment; filename=\"%s\"", fileToDownload.getName());
+         response.setHeader(headerKey, headerValue);
+
+         // obtains response's output stream
+         OutputStream outStream = response.getOutputStream();
+
+         byte[] buffer = new byte[4096];
+         int bytesRead = -1;
+
+         while ((bytesRead = in.read(buffer)) != -1) {
+             outStream.write(buffer, 0, bytesRead);
+         }
+
+         in.close();
+         outStream.close();
+
+         System.out.println("File downloaded at client successfully");
+
+
+     } catch (Exception ex) {
+         System.out.println(ex);
+     }
+ }
 }
