f1686f108d82def51ab9a9514e9513809e756b1d
minhnx <minh.nx@isofh.com>
2019-11-23 13:25:37 +0700

    Create BrandRepositoty.java

---
+++ src/main/java/com/nxm/repository/BrandRepositoty.java
@@ -0,0 +1,9 @@
+package com.nxm.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nxm.model.Brand;
+
+public interface BrandRepositoty  extends JpaRepository<Brand, Long>{
+
+}
