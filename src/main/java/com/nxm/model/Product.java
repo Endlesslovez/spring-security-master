a85787ddabf8a56e1e8a097cc85ff66734d34c9e
minhnx <minh.nx@isofh.com>
2019-11-26 15:00:00 +0700

    Update Product.java

--- src/main/java/com/nxm/model/Product.java
+++ src/main/java/com/nxm/model/Product.java
@@ -4,17 +4,23 @@ import javax.persistence.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Set;
 
 @Entity(name = "tbl_product")
-public class Product {
+public class Product implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(unique = true, nullable = false)
-	private Long id;
+	private long id;
 
 	@Column(name = "col_name")
 	private String name;
@@ -35,32 +41,27 @@ public class Product {
 	@Column(name = "col_updatedate")
 	private String updateDate;
 
-	@Column(name = "col_create_id")
-	private long userId;
-
-	@Column(name = "col_image")
-	private String image;
-
 	
 	@PrePersist
 	public void prePersist() {
 		LocalDate formattedString = LocalDate.now();
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		createDate = formattedString.format(formatter);
 
 		LocalDate formattedString1 = LocalDate.now();
-		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 		updateDate = formattedString1.format(formatter2);
 	}
 
 	@PreUpdate
 	public void preUpdate() {
 		LocalDate formattedString1 = LocalDate.now();
-		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd MMMM yyyy");
+		DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd-MMMM-yyyy");
 		updateDate = formattedString1.format(formatter2);
 	}
 
-	private long packageType;
+	@Column(name = "col_packagetype")
+	private String packageType;
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "product")
 	private Set<StockTradeDetail> stockTradeDetails;
@@ -119,45 +120,37 @@ public class Product {
 	public void setMedia(Set<MediaItem> media) {
 		this.media = media;
 	}
+//
+//	public String getCreateDate() {
+//		return createDate;
+//	}
+//
+//	public void setCreateDate(String createDate) {
+//		this.createDate = createDate;
+//	}
+//
+//	public String getUpdateDate() {
+//		return updateDate;
+//	}
+//
+//	public void setUpdateDate(String updateDate) {
+//		this.updateDate = updateDate;
+//	}
 
-	public String getCreateDate() {
-		return createDate;
-	}
+//
+//	public String getImage() {
+//		return image;
+//	}
+//
+//	public void setImage(String image) {
+//		this.image = image;
+//	}
 
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-
-	public String getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
-
-	public long getUserId() {
-		return userId;
-	}
-
-	public void setUserId(long userId) {
-		this.userId = userId;
-	}
-
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
-
-	public long getPackageType() {
+	public String getPackageType() {
 		return packageType;
 	}
 
-	public void setPackageType(long packageType) {
+	public void setPackageType(String packageType) {
 		this.packageType = packageType;
 	}
 
@@ -209,21 +202,19 @@ public class Product {
 		this.productType = productType;
 	}
 
-	public Product(Long id, String name, Brand brand, int price, Set<MediaItem> media, String createDate,
-			String updateDate, long userId, String image, long packageType,
-			Set<StockTradeDetail> stockTradeDetails, Set<StockTotalDetail> stockTotalDetails,
-			Set<WareHouseDay> wareHouseDays, Set<MediaItem> mediaItems, Set<StockChange> stockChanges,
-			ProductType productType) {
+	public Product(long id, String name, Brand brand, int price, Set<MediaItem> media,  String packageType, Set<StockTradeDetail> stockTradeDetails,
+			Set<StockTotalDetail> stockTotalDetails, Set<WareHouseDay> wareHouseDays, Set<MediaItem> mediaItems,
+			Set<StockChange> stockChanges, ProductType productType) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.brand = brand;
 		this.price = price;
 		this.media = media;
-		this.createDate = createDate;
-		this.updateDate = updateDate;
-		this.userId = userId;
-		this.image = image;
+//		this.createDate = createDate;
+//		this.updateDate = updateDate;
+//		this.userId = userId;
+//		this.image = image;
 		this.packageType = packageType;
 		this.stockTradeDetails = stockTradeDetails;
 		this.stockTotalDetails = stockTotalDetails;
@@ -236,8 +227,5 @@ public class Product {
 	public Product() {
 		super();
 	}
-
-
-	
 
 }
