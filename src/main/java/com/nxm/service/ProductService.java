ec3c8dd89c72139ea9e6c7f84104433864ab23d0
minhnx <minh.nx@isofh.com>
2019-11-26 15:00:24 +0700

    Update ProductService.java

--- src/main/java/com/nxm/service/ProductService.java
+++ src/main/java/com/nxm/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.nxm.service;
 
+import java.util.List;
+
 import com.nxm.model.Product;
 
 public interface ProductService  {
 	
-	public Product create(Product product);
+	public boolean create(Product product);
 	
 	public boolean edit(long id);
 
+	public List<Product> findAll();
 }
