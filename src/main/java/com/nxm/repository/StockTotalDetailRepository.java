178c01ca2fbbd39487592c63b9b502692d74f15c
minhnx <minh.nx@isofh.com>
2019-11-26 15:32:16 +0700

    Update StockTotalDetailRepository.java

--- src/main/java/com/nxm/repository/StockTotalDetailRepository.java
+++ src/main/java/com/nxm/repository/StockTotalDetailRepository.java
@@ -2,9 +2,10 @@ package com.nxm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.nxm.model.StockTotalDetail;
-
+@Repository
 public interface StockTotalDetailRepository extends JpaRepository<StockTotalDetail, Long> {
 	
 	
