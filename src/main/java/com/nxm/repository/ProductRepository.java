5d5ddd49312a547e6d3786713af15c7cc9b2ed13
minhnx <minh.nx@isofh.com>
2019-11-26 15:00:09 +0700

    Update ProductRepository.java

--- src/main/java/com/nxm/repository/ProductRepository.java
+++ src/main/java/com/nxm/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.nxm.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
 
 import com.nxm.model.Product;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-public interface ProductRepository extends CrudRepository<Product, Integer> {
 
 	
-}
+ }
