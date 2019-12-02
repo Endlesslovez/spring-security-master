6f0bd7ed483ee28418426056609a3c37762726c1
minhnx <minh.nx@isofh.com>
2019-12-02 13:31:52 +0700

    Update StockTotalDetailServiceImpl.java

--- src/main/java/com/nxm/service/StockTotalDetailServiceImpl.java
+++ src/main/java/com/nxm/service/StockTotalDetailServiceImpl.java
@@ -1,12 +1,16 @@
 package com.nxm.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.nxm.model.StockTotal;
 import com.nxm.model.StockTotalDetail;
 import com.nxm.repository.StockTotalDetailRepository;
+import com.nxm.repository.StockTotalRepository;
 
 @Service("stockTotalDetailService")
 public class StockTotalDetailServiceImpl implements StockTotalDetailService {
@@ -15,6 +19,20 @@ public class StockTotalDetailServiceImpl implements StockTotalDetailService {
 	@Autowired
 	private StockTotalDetailRepository stockTotalDetailRepository;
 
+	@Override
+	public Page<StockTotalDetail> findAll(Pageable pageable){
+		 return stockTotalDetailRepository.findAll(pageable);
+	 }
 
-	public Page<StockTotalDetail> findAll(Pageable pageable){return  stockTotalDetailRepository.findAll(pageable);}
+	@Override
+	public StockTotalDetail findOne(Long id) {
+		// TODO Auto-generated method stub
+		return stockTotalDetailRepository.findOne(id);
+	}
+
+	@Override
+	public List<StockTotalDetail> findByStockTotalId(Long id) {
+		// TODO Auto-generated method stub
+		return stockTotalDetailRepository.findByStockTotalId(id);
+	}
 }
