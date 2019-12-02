77abf0ed4b915f306190a7ab2b2edcdc58c2c4c4
minhnx <minh.nx@isofh.com>
2019-12-02 13:31:49 +0700

    Update StockTotalDetailService.java

--- src/main/java/com/nxm/service/StockTotalDetailService.java
+++ src/main/java/com/nxm/service/StockTotalDetailService.java
@@ -1,5 +1,7 @@
 package com.nxm.service;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -7,4 +9,6 @@ import com.nxm.model.StockTotalDetail;
 
 public interface StockTotalDetailService {
     Page<StockTotalDetail> findAll(Pageable pageable);
+    StockTotalDetail findOne(Long id);
+    List<StockTotalDetail>  findByStockTotalId(Long id);
 }
